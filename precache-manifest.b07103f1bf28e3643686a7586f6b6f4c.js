ec8006243a45c3b2a279b96e977b9a6767c780f8
Dnyaneshwar B. Kanawade <43498173+dnyanu2713@users.noreply.github.com>
2020-01-09 20:23:17 +0530

    index and json file uploaded

---
+++ precache-manifest.b07103f1bf28e3643686a7586f6b6f4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d199dbeaedb6cf6f546d29d6aec95dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c4fa16b742b0f3ec81",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a47454145cc746b41bf8",
+    "url": "/static/css/main.89497188.chunk.css"
+  },
+  {
+    "revision": "c8c4fa16b742b0f3ec81",
+    "url": "/static/js/2.2eb28435.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2eb28435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a47454145cc746b41bf8",
+    "url": "/static/js/main.62abc124.chunk.js"
+  },
+  {
+    "revision": "aaf73407136352df0786",
+    "url": "/static/js/runtime-main.86bd4872.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
